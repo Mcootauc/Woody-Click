3a903fc5908dda83ac45c7b5db19e9ba2a8117d8
Aiden McDougald <aidenmcdougald@gmail.com>
2023-04-15 14:58:19 -0700

    Pulls recent donos but thats broken for some reason

--- TeamTreesScraper.py
+++ TeamTreesScraper.py
@@ -6,6 +6,8 @@ url = "https://teamtrees.org/"
 
 response = requests.get((url))
 
-parsed = BeautifulSoup(response.text, "lxml")
+parsed = BeautifulSoup(response.text, "html.parser")
 
-print(parsed.find(class_="max-w-screen-sm w-full mx-auto").find_all(class_=)
+recentDonos = parsed.find(class_="max-w-screen-sm w-full mx-auto").find_all(class_="w-full bg-white shadow  rounded-md relative mt-4 flex flex-row ", limit=10)
+
+print(recentDonos)
