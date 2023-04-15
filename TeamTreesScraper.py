91cad90380b09d3604a50f64c59923be9d5c1fae
Aiden McDougald <aidenmcdougald@gmail.com>
2023-04-15 15:17:50 -0700

    trying to datastructure it but it broken

--- TeamTreesScraper.py
+++ TeamTreesScraper.py
@@ -6,8 +6,18 @@ url = "https://teamtrees.org/"
 
 response = requests.get((url))
 
+donoDic = {}
+
 parsed = BeautifulSoup(response.text, "html.parser")
 
-recentDonos = parsed.find(class_="max-w-screen-sm w-full mx-auto").find_all(class_="w-full bg-white shadow  rounded-md relative mt-4 flex flex-row ", limit=10)
+for i in range(10):
+    if i == 0:
+        continue
+    treeTimestamp = []
+    dono = parsed.find(class_="max-w-screen-sm w-full mx-auto").contents[i]
+    treeTimestamp.append(dono.find(class_="mt-0 md:mt-4 bg-lightMoss rounded-full text-white text-bold px-4 relative badge").get_text())
+    treeTimestamp.append(dono.find(class_="text-center text-xs mt-2 opacity-50 feed-datetime").get_text())
 
-print(recentDonos)
+    donoDic[dono.find(class_="text-spruce font-black text-lg")] = treeTimestamp
+
+print(donoDic)
