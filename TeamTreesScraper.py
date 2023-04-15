dd3d204d20e27d8c2f8fb2776ee0380d686bafa9
Mitchell Cootauco <mcootauc@lion.lmu.edu>
2023-04-15 15:00:04 -0700

    merged

--- TeamTreesScraper.py
+++ TeamTreesScraper.py
@@ -11,5 +11,3 @@ parsed = BeautifulSoup(response.text, "html.parser")
 recentDonos = parsed.find(class_="max-w-screen-sm w-full mx-auto").find_all(class_="w-full bg-white shadow  rounded-md relative mt-4 flex flex-row ", limit=10)
 
 print(recentDonos)
-
-print(recentDonos)
