669bc907b3741d0f4263aa94c0fbe67f1d84f06a
Mitchell Cootauco <mcootauc@lion.lmu.edu>
2023-04-15 16:20:51 -0700

    donos are now individual dicts and added to list

--- TeamTreesScraper.py
+++ TeamTreesScraper.py
@@ -21,14 +21,19 @@ parsed = BeautifulSoup(response.text, "html.parser")
 
 donos = parsed.find(class_="max-w-screen-sm w-full mx-auto")
 
-donoDic = {}
-for dono in donos.find_all(class_="w-full bg-white shadow rounded-md relative mt-4 flex flex-row", limit=2):
+donoList = []
+for dono in donos.find_all(class_="w-full bg-white shadow rounded-md relative mt-4 flex flex-row", limit=5):
     treeTimestamp = []
+    donoDic = {}
+    
     #appends the number of trees
     treeTimestamp.append(dono.find(class_="mt-0 md:mt-4 bg-lightMoss rounded-full text-white text-bold px-4 relative badge").get_text().split(" ")[0])
     #appends the timestamp
     treeTimestamp.append(dono.find(class_="text-center text-xs mt-2 opacity-50 feed-datetime").get_text())
 
-    #adds the dono name with the number of trees and timestamp to the dictionary
+    #adds the individual dono name with the number of trees and timestamp to the dictionary
     donoDic[dono.find(class_="text-spruce font-black text-lg").get_text()] = treeTimestamp
 
+    donoList.append(donoDic)
+
+print(donoList)
