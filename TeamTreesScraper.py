294aebeb659252bbf01034e392fae4b918ca09ee
Aiden McDougald <aidenmcdougald@gmail.com>
2023-04-15 15:39:32 -0700

    fixed the parsing kinda

--- TeamTreesScraper.py
+++ TeamTreesScraper.py
@@ -10,14 +10,18 @@ donoDic = {}
 
 parsed = BeautifulSoup(response.text, "html.parser")
 
-for i in range(10):
-    if i == 0:
+donos = parsed.find(class_="max-w-screen-sm w-full mx-auto")
+
+first = True
+
+for dono in donos.find_all(class_="w-full bg-white shadow rounded-md relative mt-4 flex flex-row", limit=2):
+    if first:
+        first = False
         continue
     treeTimestamp = []
-    dono = parsed.find(class_="max-w-screen-sm w-full mx-auto").contents[i]
-    treeTimestamp.append(dono.find(class_="mt-0 md:mt-4 bg-lightMoss rounded-full text-white text-bold px-4 relative badge").get_text())
-    treeTimestamp.append(dono.find(class_="text-center text-xs mt-2 opacity-50 feed-datetime").get_text())
 
-    donoDic[dono.find(class_="text-spruce font-black text-lg")] = treeTimestamp
+    print(dono.find(class_="text-spruce font-black text-lg"))
+
+    donoDic[dono.find(class_="text-spruce font-black text-lg").get_text()] = treeTimestamp
 
 print(donoDic)
