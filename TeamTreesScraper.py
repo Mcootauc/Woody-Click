c4ed35b30aef5de7bc7a0121123c5e8e24760cbd
Aiden McDougald <aidenmcdougald@gmail.com>
2023-04-15 14:39:34 -0700

    It parses:)

--- TeamTreesScraper.py
+++ TeamTreesScraper.py
@@ -2,4 +2,10 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-url = ""
+url = "https://teamtrees.org/"
+
+response = requests.get((url))
+
+parsed = BeautifulSoup(response.text, "lxml")
+
+print(parsed.find(class_="max-w-screen-sm w-full mx-auto").find_all(class_=)
