9be6d1ac466b578fa26a6bb76bee95a2e27c2e3b
Mitchell Cootauco <mcootauc@lion.lmu.edu>
2023-04-15 14:58:46 -0700

    updates

--- TeamTreesScraper.py
+++ TeamTreesScraper.py
@@ -1,4 +1,4 @@
-import requests
+import requests 
 import time
 from bs4 import BeautifulSoup
 
@@ -6,6 +6,6 @@ url = "https://teamtrees.org/"
 
 response = requests.get((url))
 
-parsed = BeautifulSoup(response.text, "lxml")
+parsed = BeautifulSoup(response.text, "html.parser")
 
-print(parsed.find(class_="max-w-screen-sm w-full mx-auto").find_all(class_=)
+print(parsed.find(class_="max-w-screen-sm w-full mx-auto"))
